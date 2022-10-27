135aab6f0b0f8c7e5c8a1d18b039dd5da5e57a63
Riccardo Taiello <ricardo.taiello@inria.fr>
2022-10-27 17:24:39 +0200

    created dev branch

---
+++ src/layer.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+import crypten
+import torch
+import math
+
+
+class Layer(ABC):
+    # code from https://towardsdatascience.com/building-neural-network-from-scratch-9c88535bf8e9
+    def __init__(self)->None:
+        # Here we can initialize layer parameters (if any) and auxiliary stuff.
+        # A dummy layer does nothing
+        pass
+
+    @abstractmethod
+    def forward(self, input: crypten.CrypTensor) -> crypten.CrypTensor:
+        # Takes input data of shape [batch, input_units], returns output data [batch, output_units]
+        pass
+
+    @abstractmethod
+    def backward(self, input: crypten.CrypTensor, grad_output: crypten.CrypTensor) -> crypten.CrypTensor:
+        # Performs a backpropagation step through the layer, with respect to the given input.
+
+        # To compute loss gradients w.r.t input, we need to apply chain rule (backprop):
+
+        pass
+
+
+class ReLU(Layer):
+    def __init__(self)->None:
+        # ReLU layer simply applies elementwise rectified linear unit to all inputs
+        super().__init__()
+
+    def forward(self, input: crypten.CrypTensor) -> crypten.CrypTensor:
+        # Apply elementwise ReLU to [batch, input_units] matrix
+        relu_forward = input.relu()  # torch.maximum(torch.zeros_like(input),input)
+        return relu_forward
+
+    def backward(self, input:crypten.CrypTensor, grad_output: crypten.CrypTensor) -> crypten.CrypTensor:
+        # Compute gradient of loss w.r.t. ReLU input
+        relu_grad = input > 0
+        return grad_output * relu_grad
+
+
+class Dense(Layer):
+    def __init__(self, input_units: int, output_units: int, learning_rate: float=0.1)->None:
+        # A dense layer is a layer which performs a learned affine transformation:
+        # f(x) = <W*x> + b
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.weights = torch.normal(mean=torch.zeros((input_units, output_units)),
+                                    std=math.sqrt(2 / (input_units + output_units)))
+        self.biases = torch.zeros(output_units)
+
+    def forward(self, input: crypten.CrypTensor) -> crypten.CrypTensor:
+        # Perform an affine transformation:
+        # f(x) = <W*x> + b
+
+        # input shape: [batch, input_units]
+        # output shape: [batch, output units]
+
+        return input.matmul(self.weights) + self.biases
+
+    def backward(self, input:crypten.CrypTensor, grad_output: crypten.CrypTensor) -> crypten.CrypTensor:
+        # compute d f / d x = d f / d dense * d dense / d x
+        # where d dense/ d x = weights transposed
+        grad_input = grad_output.matmul(self.weights.T)
+
+        # compute gradient w.r.t. weights and biases
+        grad_weights = input.transpose(0, 1).matmul(grad_output)
+        grad_biases = grad_output.mean(axis=0) * input.shape[0]
+
+        assert grad_weights.shape == self.weights.shape and grad_biases.shape == self.biases.shape
+
+        # Here we perform a stochastic gradient descent step.
+        self.weights = self.weights - self.learning_rate * grad_weights.get_plain_text()
+        self.biases = self.biases - self.learning_rate * grad_biases.get_plain_text()
+
+        return grad_input
--- src/run.py
+++ src/run.py
@@ -1,14 +1,12 @@
-from typing import Dict, List, Tuple
-
+import crypten
 import torch
 import torchvision
-import numpy as np
-import crypten
-import random
 import os
-
-from tqdm import trange
 from crypten.nn.loss import CrossEntropyLoss
+
+from src.layer import Dense, ReLU
+
+critertion = CrossEntropyLoss()
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 seed = 42
 
@@ -34,115 +32,89 @@ def load_mnist() -> (torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor):
     )
 
     # create torch.Tensor objects for the training and test data
-    x_train: torch.Tensor = train_dataset.data.reshape(-1, 28 * 28).float()
-    x_train_mean: float = x_train.mean()
-    x_train_std: float = x_train.std()
-    # Standardize the training data
-    x_train_norm = (x_train - x_train_mean) / x_train_std
+    X_train: torch.Tensor = train_dataset.data.reshape(-1, 28 * 28).float()/255
     y_train = train_dataset.targets
     # Standardize the test data
-    x_test = test_dataset.data.reshape(-1, 28 * 28).float()
-    x_test_norm = (x_test - x_train_mean) / x_train_std
+    X_test = test_dataset.data.reshape(-1, 28 * 28).float()/255
     y_test = test_dataset.targets
 
-    return x_train_norm, y_train, x_test_norm, y_test
-
-def initialize(num_inputs, num_classes) -> Dict[str, torch.Tensor]:
-    """
-    initialize the parameters
-    :param num_inputs:
-    :param num_classes:
-    :return:
-    """
-    w: torch.Tensor = torch.rand(num_inputs, num_classes, device=DEVICE)
-    b: torch.Tensor = torch.rand(1, num_classes, device=DEVICE)
-
-    param: Dict[str, torch.Tensor] = {"w": w, "b": b}  # (10*784)  # (10*1)
-    return param
+    return X_train, y_train, X_test, y_test
 
 
-def train_crypten(
-    param: Dict[str, torch.Tensor],
-    x_train: torch.Tensor,
-    y_train: torch.Tensor,
-    num_epochs: int,
-    batch_size: int,
-    learning_rate: float,
-    criterion: crypten.nn.loss,
-    verbose: bool = False,
-    num_classes: int = 10
-) -> Tuple[Dict[str, torch.Tensor], List[float]]:
-    """
-    Train the model with the given parameters
-    :param param:
-    :param x_train:
-    :param y_train:
-    :param num_epochs:
-    :param batch_size:
-    :param learning_rate:
-    :return:
-    """
-    losses: List[float] = []
-    current_loss = 0
-    for _ in trange(num_epochs, desc="Epoch"):
-        # select the random sequence of training set
-        rand_indices: List[int] = np.random.choice(
-            x_train.shape[0], x_train.shape[0], replace=False
-        )
-        num_batches: int = int(x_train.shape[0] / batch_size)
-        for batch in trange(num_batches, desc="Batch"):
-            index = rand_indices[batch * batch_size : (batch + 1) * batch_size]
-            x_batch = x_train[index]  # (batch_size, num_inputs)
-            y_batch = y_train[index]  # (batch_size, 1)
-            x_batch = crypten.cryptensor(x_batch, device=DEVICE)  # encrypt the features
-            logits = x_batch.matmul(param["w"]) + param["b"]  # (num_classes, 1)
-
-            # https://codesti.com/issue/facebookresearch/CrypTen/278
-            y_one_hot = torch.nn.functional.one_hot(y_batch, num_classes)
-            if verbose:
-                loss = criterion(logits, y_one_hot)
-                current_loss = loss.get_plain_text()
-                print(current_loss)
-            # https://aaronkub.com/2020/02/12/logistic-regression-with-pytorch.html#why-logistic-regression
-            activation = logits.softmax(1)
-            w_gradients = (
-                -x_batch.transpose(0, 1).matmul(-activation + y_one_hot) / batch_size
-            )  # (num_inputs, num_classes)
-            b_gradients = -(-activation + y_one_hot).mean(0)
-            param["w"] -= learning_rate * w_gradients.get_plain_text()
-            param["b"] -= learning_rate * b_gradients.get_plain_text()
-        losses.append(current_loss)
-    return param, losses
 
 
-def evaluation(
-    param: Dict[str, torch.Tensor], x: torch.Tensor, y: torch.Tensor
-) -> None:
-    test_predictions = torch.argmax(
-        torch.softmax(x.matmul(param["w"]) + param["b"], dim=1), dim=1
-    )
-    test_accuracy = torch.sum(test_predictions == y).float() / y.shape[0]
-    print(f"Accuracy: {test_accuracy}")
+
+def softmax_crossentropy_with_logits(logits: crypten.CrypTensor, reference_answers):
+    # Compute crossentropy from logits[batch,n_classes] and ids of correct answers
+    one_hot = torch.nn.functional.one_hot(reference_answers, 10)
+    # logits_for_answers = logits[torch.arange(len(logits)),reference_answers]
+
+    # xentropy = - logits_for_answers + torch.log(torch.sum(torch.exp(logits),axis=-1))
+    xentropy = critertion(logits, one_hot)
+
+    return xentropy
 
 
-def run_lr_mnist(num_epochs, learning_rate, batch_size):
-    crypten.init()
-    if seed is not None:
-        random.seed(seed)
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-    criterion = CrossEntropyLoss()
+def grad_softmax_crossentropy_with_logits(logits, reference_answers):
+    # Compute crossentropy gradient from logits[batch,n_classes] and ids of correct answers
+    # ones_for_answers = torch.zeros_like(logits)
+    # ones_for_answers[torch.arange(len(logits)),reference_answers] = 1
+    one_hot = torch.nn.functional.one_hot(reference_answers, 10)
 
-    x_train, y_train, x_test, y_test = load_mnist()
-    x_train = x_train.to(DEVICE)
-    y_train = y_train.to(DEVICE)
-    x_test = x_test.to(DEVICE)
-    y_test = y_test.to(DEVICE)
-    num_inputs = x_train.shape[1]
-    num_classes = len(torch.unique(y_train))
-    param = initialize(num_inputs, num_classes)
-    param, _ =  train_crypten(param=param, x_train=x_train, y_train=y_train,num_epochs=num_epochs, batch_size=batch_size, learning_rate=learning_rate, criterion=criterion,num_classes=num_classes, verbose=False)
-    print("Evaluation training over encrypted features")
-    evaluation(param, x_test, y_test)
+    softmax = logits.softmax(1)  # torch.exp(logits) / torch.exp(logits).sum(axis=-1,keepdims=True)
+
+    return (softmax - one_hot) / logits.shape[0]
 
 
+def forward(network, X):
+    # Compute activations of all network layers by applying them sequentially.
+    # Return a list of activations for each layer.
+
+    activations = []
+    input = X
+    # Looping through each layer
+    for l in network:
+        activations.append(l.forward(input))
+        # Updating input to last layer output
+        input = activations[-1]
+
+    assert len(activations) == len(network)
+    return activations
+
+
+def predict(network, X):
+    # Compute network predictions. Returning indices of largest Logit probability
+    logits = forward(network, X)[-1]
+    return logits.argmax(axis=-1)
+
+
+def train(network, X, y):
+    # Train our network on a given batch of X and y.
+    # We first need to run forward to get all layer activations.
+    # Then we can run layer.backward going from last to first layer.
+    # After we have called backward for all layers, all Dense layers have already made one gradient step.
+
+    # Get the layer activations
+    layer_activations = forward(network, X)
+    layer_inputs = [X] + layer_activations  # layer_input[i] is an input for network[i]
+    logits = layer_activations[-1]
+
+    # Compute the loss and the initial gradient
+    loss = softmax_crossentropy_with_logits(logits, y)
+    loss_grad = grad_softmax_crossentropy_with_logits(logits, y)
+
+    # Propagate gradients through the network
+    # Reverse propogation as this is backprop
+    for layer_index in range(len(network))[::-1]:
+        layer = network[layer_index]
+
+        loss_grad = layer.backward(layer_inputs[layer_index], loss_grad)  # grad w.r.t. input, also weight updates
+    return loss  # torch.mean(loss)
+
+X_train, y_train, X_test, y_test = load_mnist()
+network = []
+network.append(Dense(X_train.shape[1], 100))
+network.append(ReLU())
+network.append(Dense(100, 200))
+network.append(ReLU())
+network.append(Dense(200, 10))
